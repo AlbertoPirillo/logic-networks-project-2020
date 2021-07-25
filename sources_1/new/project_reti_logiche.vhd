5a1355269670d48592fb91fd4320b3f70aa042c1
Alberto Pirillo <alberto.pirillo@mail.polimi.it>
2021-07-25 21:57:10 +0200

    Added shift_level calculation

--- sources_1/new/project_reti_logiche.vhd
+++ sources_1/new/project_reti_logiche.vhd
@@ -50,18 +50,65 @@ end project_reti_logiche;
 
 
 
+
 architecture Behavioral of project_reti_logiche is
 
 begin
 
+    -- BEST PRACTICE: std_logic_vector => unsigned => integer (=> back to std_logic_vector) 
     -- TODO: lista di sensibilità
     process(i_clk)
         variable N_COLUMN, N_ROW : std_logic;
         variable MAX_PIXEL_VALUE, MIN_PIXEL_VALUE, OUT_BEGIN : integer;
+        variable DELTA_VALUE, SHIFT_LEVEL, TEMP_PIXEL : integer;
     begin
         N_COLUMN := i_data(0);
         N_ROW := i_data(1);
         OUT_BEGIN := 2 + (integer(N_COLUMN) * integer(N_ROW));
-    end process;
+        
+        
+        -- Find highest and the lowest value between all pixels
+        MAX_PIXEL_VALUE := integer(i_data(2));
+        MIN_PIXEL_VALUE := integer(i_data(2));
+        
+        for i in 2 to (OUT_BEGIN - 1) loop   
+            if integer(i_data(i)) < MIN_PIXEL_VALUE then
+                MIN_PIXEL_VALUE := integer(i_data(i));
+            elsif integer(i_data(i)) > MAX_PIXEL_VALUE then
+                MAX_PIXEL_VALUE := integer(i_data(i));
+            end if;
+        end loop;     
+        
+        --Perform the equalization on each pixel
+        DELTA_VALUE := MAX_PIXEL_VALUE - MIN_PIXEL_VALUE;
+        for i in 2 to (OUT_BEGIN - 1) loop
+            -- Compute SHIFT_LEVEL by threshold discretization
+            if DELTA_VALUE = 0 then
+                SHIFT_LEVEL := 8;     
+            elsif DELTA_VALUE >= 1 AND DELTA_VALUE < 3 then
+                SHIFT_LEVEL := 7;      
+            elsif DELTA_VALUE >= 3 AND DELTA_VALUE < 7 then
+                SHIFT_LEVEL := 6;
+            elsif DELTA_VALUE >= 7 AND DELTA_VALUE < 15 then
+                SHIFT_LEVEL := 5;
+            elsif DELTA_VALUE >= 15 AND DELTA_VALUE < 31 then
+                SHIFT_LEVEL := 4;
+            elsif DELTA_VALUE >= 31 AND DELTA_VALUE < 63 then
+                SHIFT_LEVEL := 3;
+            elsif DELTA_VALUE >= 63 AND DELTA_VALUE < 127 then
+                SHIFT_LEVEL := 2;
+            elsif DELTA_VALUE >= 127 AND DELTA_VALUE < 255 then
+                SHIFT_LEVEL := 1;
+            elsif DELTA_VALUE = 255 then
+                SHIFT_LEVEL := 0;   
+            end if;    
+
+--            TEMP_PIXEL = (CURRENT_PIXEL_VALUE - MIN_PIXEL_VALUE) << SHIFT_LEVEL;
+--            NEW_PIXEL_VALUE = MIN( 255 , TEMP_PIXEL);
+
+
+        end loop;
+        
+   end process;
     
 end Behavioral;
