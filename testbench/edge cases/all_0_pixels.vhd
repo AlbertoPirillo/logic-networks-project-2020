d6f8ff9e22c22b9576a3f12b5d6b50c8134f6c05
Alberto Pirillo <alberto.pirillo@mail.polimi.it>
2021-08-18 19:40:56 +0200

    Updated report with simulations

--- testbench/edge cases/all_0_pixels.vhd
+++ testbench/edge cases/all_0_pixels.vhd
@@ -8,7 +8,7 @@ entity project_tb is
 end project_tb;
 
 architecture projecttb of project_tb is
-constant c_CLOCK_PERIOD         : time := 550 ns;
+constant c_CLOCK_PERIOD         : time := 15 ns;
 signal   tb_done                : std_logic;
 signal   mem_address            : std_logic_vector (15 downto 0) := (others => '0');
 signal   tb_rst                 : std_logic := '0';
@@ -21,9 +21,16 @@ signal   mem_we                 : std_logic;
 type ram_type is array (65535 downto 0) of std_logic_vector(7 downto 0);
 
 
-signal RAM: ram_type := (0 => std_logic_vector(to_unsigned(  0  , 8)), 
-                         1 => std_logic_vector(to_unsigned(  0  , 8)), 
-                         2 => std_logic_vector(to_unsigned(  46  , 8)),
+signal RAM: ram_type := (0 => std_logic_vector(to_unsigned(  4  , 8)), 
+                         1 => std_logic_vector(to_unsigned(  2  , 8)), 
+                         2 => std_logic_vector(to_unsigned(  0  , 8)), 
+                         3 => std_logic_vector(to_unsigned(  0  , 8)),  
+                         4 => std_logic_vector(to_unsigned(  0 , 8)), 
+                         5 => std_logic_vector(to_unsigned(  0  , 8)), 
+                         6 => std_logic_vector(to_unsigned(  0  , 8)), 
+                         7 => std_logic_vector(to_unsigned(  0  , 8)), 
+                         8 => std_logic_vector(to_unsigned(  0  , 8)), 
+                         9 => std_logic_vector(to_unsigned(  0  , 8)), 
                          others => (others =>'0'));         
 			 -- Expected Output  6 -> 0                         
 			 -- Expected Output  7 -> 255                         
@@ -102,7 +109,14 @@ begin
     -- Immagine originale =  [46, 131, 62, 89]  
     -- Immagine di output =  [0, 255, 64, 172]  
     
-    assert RAM(2) = std_logic_vector(to_unsigned( 46 , 8)) report "TEST FALLITO (WORKING ZONE). Expected  0  found " & integer'image(to_integer(unsigned(RAM(6))))  severity failure;
+    assert RAM(10) = std_logic_vector(to_unsigned( 0 , 8)) report "TEST FALLITO (WORKING ZONE). Expected  0  found " & integer'image(to_integer(unsigned(RAM(6))))  severity failure;
+    assert RAM(11) = std_logic_vector(to_unsigned( 0 , 8)) report "TEST FALLITO (WORKING ZONE). Expected  0  found " & integer'image(to_integer(unsigned(RAM(6))))  severity failure;
+    assert RAM(12) = std_logic_vector(to_unsigned( 0 , 8)) report "TEST FALLITO (WORKING ZONE). Expected  0  found " & integer'image(to_integer(unsigned(RAM(6))))  severity failure;
+    assert RAM(13) = std_logic_vector(to_unsigned( 0 , 8)) report "TEST FALLITO (WORKING ZONE). Expected  0  found " & integer'image(to_integer(unsigned(RAM(6))))  severity failure;
+    assert RAM(14) = std_logic_vector(to_unsigned( 0 , 8)) report "TEST FALLITO (WORKING ZONE). Expected  0  found " & integer'image(to_integer(unsigned(RAM(6))))  severity failure;
+    assert RAM(15) = std_logic_vector(to_unsigned( 0 , 8)) report "TEST FALLITO (WORKING ZONE). Expected  0  found " & integer'image(to_integer(unsigned(RAM(6))))  severity failure;
+    assert RAM(16) = std_logic_vector(to_unsigned( 0 , 8)) report "TEST FALLITO (WORKING ZONE). Expected  0  found " & integer'image(to_integer(unsigned(RAM(6))))  severity failure;
+    assert RAM(17) = std_logic_vector(to_unsigned( 0 , 8)) report "TEST FALLITO (WORKING ZONE). Expected  0  found " & integer'image(to_integer(unsigned(RAM(6))))  severity failure;
     
     assert false report "Simulation Ended! TEST PASSATO" severity failure;
 end process test;
